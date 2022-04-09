7a759feb0a47b101ea34f29904b94c5b111ea12d
sean-udayantha <seanudayantha@mail.com>
2022-04-10 00:16:23 +0530

    test commit

--- client/src/components/Register/StaffRegister/Register.js
+++ client/src/components/Register/StaffRegister/Register.js
@@ -20,6 +20,7 @@ const Register = () => {
   const [visible2, setVisible2] = useState(false);
   const [supervisor, setSupervisor] = useState(false);
   const [cosupervisor, setCoSupervisor] = useState(false);
+  const [degree, setDegree] = useState(null);
   const [checkedComputing, setCheckedComputing] = useState(false);
   
 
