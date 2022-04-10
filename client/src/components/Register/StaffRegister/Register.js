f6a690a88eabac7a3974501ccf87fe06dd539266
sean-udayantha <seanudayantha@mail.com>
2022-04-10 11:22:39 +0530

    edit SatffRegister

--- client/src/components/Register/StaffRegister/Register.js
+++ client/src/components/Register/StaffRegister/Register.js
@@ -48,7 +48,7 @@ const Register = () => {
       },
     },
   };
-  
+
   const computingTopics = [
     'Information Security',
     'Artificial Intelligence and Machine Learning',
@@ -67,7 +67,7 @@ const Register = () => {
     'Human Computer Interaction',
 
   ];
-  
+
   function getStyles(name, ComputerTopics, theme) {
     return {
       fontWeight:
@@ -76,14 +76,14 @@ const Register = () => {
           : theme.typography.fontWeightMedium,
     };
   }
-  
 
 
-  
+
+
 
   const handleOnChangeComputing = () => {
     setCheckedComputing(!checkedComputing);
-    if(checkedComputing==false){
+    if (checkedComputing == false) {
       setComputerTopics([]);
     }
   };
@@ -101,174 +101,174 @@ const Register = () => {
 
   return (
     <>
-    <div className="reg">
-    <h1>
-        Staff Registration
-    </h1>
-   
-    <form className="" novalidate>
+      <div className="reg">
+        <h1>
+          Staff Registration
+        </h1>
 
-    <div className="column">
-    <label htmlFor="validationCustom04" className="form-label" >Your Role</label>
-    <select className="form-select"  onChange={handleStaffChange} id="validationCustom04" required>
-      <option selected disabled value="">Choose Your Role...</option>
-      <option value="supervisor">Supervisor</option>
-      <option value="coSupervisor">Co-Supervisor</option>
-    </select>
-    
-  </div>
+        <form className="" novalidate>
 
-  <div className="column">
-    <label htmlFor="validationCustom01" className="form-label">Name with Inisials</label>
-    <input type="text" className="form-control" id="validationCustom01"  required/>
-    
-  </div>
-  <div className="column">
-    <label htmlFor="validationCustom04" className="form-label">Gender</label>
-    <select className="form-select" id="validationCustom04" required>
-      <option selected disabled value="">Choose Your Gender...</option>
-      <option>Male</option>
-      <option>Female</option>
-    </select>
-    
-  </div>
-  <div className="column">
-    <label htmlFor="validationCustom02" className="form-label">Username</label>
-    <input type="text" className="form-control" id="validationCustomUsername" required/>
-   
-  </div>
-  
+          <div className="column">
+            <label htmlFor="validationCustom04" className="form-label" >Your Role</label>
+            <select className="form-select" onChange={handleStaffChange} id="validationCustom04" required>
+              <option selected disabled value="">Choose Your Role...</option>
+              <option value="supervisor">Supervisor</option>
+              <option value="coSupervisor">Co-Supervisor</option>
+            </select>
 
+          </div>
 
-  
-  
-  <div className="column">
-    <label htmlFor="validationCustom03" className="form-label">Email</label>
-    <input type="email" className="form-control" id="validationCustom03" required/>
-    
-  </div>
+          <div className="column">
+            <label htmlFor="validationCustom01" className="form-label">Name with Inisials</label>
+            <input type="text" className="form-control" id="validationCustom01" required />
 
-  <div className="column">
-  <label htmlFor="exampleFormControlTextarea1" class="form-label">About me</label>
-  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-    
-  </div>
+          </div>
+          <div className="column">
+            <label htmlFor="validationCustom04" className="form-label">Gender</label>
+            <select className="form-select" id="validationCustom04" required>
+              <option selected disabled value="">Choose Your Gender...</option>
+              <option>Male</option>
+              <option>Female</option>
+            </select>
 
-  <div className="column">
-  <label htmlFor="" class="form-label">Interested Topics</label>
-  <div className="form-check">
-  <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" 
-  checked={checkedComputing}
-  onChange={handleOnChangeComputing}/>
-  <label className="form-check-label" for="flexCheckDefault">
-    Computing
-  </label>
-  <div className="column">{checkedComputing &&  <> 
-  
-    <div>
-      
-        <InputLabel id="demo-multiple-chip-label">Select your Computing topics</InputLabel>
-        <Select
-          labelId="demo-multiple-chip-label"
-          id="demo-multiple-chip"
-          multiple
-          value={ComputerTopics}
-          onChange={handleChange}
-          input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-          renderValue={(selected) => (
-            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-              {selected.map((value) => (
-                <Chip key={value} label={value} />
-              ))}
-            </Box>
-          )}
-          MenuProps={MenuProps}
-        >
-          {computingTopics.map((name) => (
-            <MenuItem
-              key={name}
-              value={name}
-              style={getStyles(name, ComputerTopics, theme)}
-            >
-              {name}
-            </MenuItem>
-          ))}
-        </Select>
-      
-    </div>
-  
-   </>}
-  
- 
+          </div>
+          <div className="column">
+            <label htmlFor="validationCustom02" className="form-label">Username</label>
+            <input type="text" className="form-control" id="validationCustomUsername" required />
 
-  </div>
-  
-</div>
+          </div>
 
 
-  
 
-<div className="form-check">
-  <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-  <label className="form-check-label" for="flexCheckDefault">
-    Bussines
-  </label>
-  
-</div>
-<div className="form-check">
-  <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-  <label className="form-check-label" for="flexCheckDefault">
-    Engineering
-  </label>
-  
-</div>
-    
-  </div>
-  <div className="column">
- 
-  
-  </div>
 
-  
- 
-  <div className="column">
-  <label htmlFor="validationCustom05" className="form-label">Password</label>
-  <div className="input-group has-validation">
-      <input className="form-control" id="validationCustom05" type={visible ? "text" : "password"}
-        text="Password"
-        required/>
-        <span className="input-group-text" id="validationTooltipUsernamePrepend">
-          <div onClick={handleClick}>{visible ? <MdVisibility /> : <MdVisibilityOff />}</div></span>
-    </div>
-  </div>
 
-  <div className="column">
-  <label htmlFor="validationCustom05" className="form-label">Confirm Password</label>
-  <div className="input-group has-validation">
-      <input className="form-control" id="validationCustom05" type={visible2 ? "text" : "password"}
-        text="Password"
-        required/>
-        <span className="input-group-text" id="validationTooltipUsernamePrepend">
-          <div onClick={handleClick2}>{visible2 ? <MdVisibility /> : <MdVisibilityOff />}</div></span>
-    </div>
-  </div>
+          <div className="column">
+            <label htmlFor="validationCustom03" className="form-label">Email</label>
+            <input type="email" className="form-control" id="validationCustom03" required />
 
-  <div className="column">
-    <div className="form-check">
-      <input className="form-check-input" type="checkbox" value="" id="invalidCheck" required/>
-      <label className="form-check-label" for="invalidCheck">
-        Agree to terms and conditions
-      </label>
-      
-    </div>
-  </div>
-  <div className="column">
-  <div className="login_btn">
-    <button type="submit">register</button>
-  </div>
-  </div>
-</form>
-</div>
+          </div>
+
+          <div className="column">
+            <label htmlFor="exampleFormControlTextarea1" class="form-label">About me</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+
+          </div>
+
+          <div className="column">
+            <label htmlFor="" class="form-label">Interested Topics</label>
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"
+                checked={checkedComputing}
+                onChange={handleOnChangeComputing} />
+              <label className="form-check-label" for="flexCheckDefault">
+                Computing
+              </label>
+              <div className="column">{checkedComputing && <>
+
+                <div>
+
+                  <InputLabel id="demo-multiple-chip-label">Select your Computing topics</InputLabel>
+                  <Select
+                    labelId="demo-multiple-chip-label"
+                    id="demo-multiple-chip"
+                    multiple
+                    value={ComputerTopics}
+                    onChange={handleChange}
+                    input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+                    renderValue={(selected) => (
+                      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                        {selected.map((value) => (
+                          <Chip key={value} label={value} />
+                        ))}
+                      </Box>
+                    )}
+                    MenuProps={MenuProps}
+                  >
+                    {computingTopics.map((name) => (
+                      <MenuItem
+                        key={name}
+                        value={name}
+                        style={getStyles(name, ComputerTopics, theme)}
+                      >
+                        {name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+
+                </div>
+
+              </>}
+
+
+
+              </div>
+
+            </div>
+
+
+
+
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+              <label className="form-check-label" for="flexCheckDefault">
+                Bussines
+              </label>
+
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+              <label className="form-check-label" for="flexCheckDefault">
+                Engineering
+              </label>
+
+            </div>
+
+          </div>
+          <div className="column">
+
+
+          </div>
+
+
+
+          <div className="column">
+            <label htmlFor="validationCustom05" className="form-label">Password</label>
+            <div className="input-group has-validation">
+              <input className="form-control" id="validationCustom05" type={visible ? "text" : "password"}
+                text="Password"
+                required />
+              <span className="input-group-text" id="validationTooltipUsernamePrepend">
+                <div onClick={handleClick}>{visible ? <MdVisibility /> : <MdVisibilityOff />}</div></span>
+            </div>
+          </div>
+
+          <div className="column">
+            <label htmlFor="validationCustom05" className="form-label">Confirm Password</label>
+            <div className="input-group has-validation">
+              <input className="form-control" id="validationCustom05" type={visible2 ? "text" : "password"}
+                text="Password"
+                required />
+              <span className="input-group-text" id="validationTooltipUsernamePrepend">
+                <div onClick={handleClick2}>{visible2 ? <MdVisibility /> : <MdVisibilityOff />}</div></span>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="invalidCheck" required />
+              <label className="form-check-label" for="invalidCheck">
+                Agree to terms and conditions
+              </label>
+
+            </div>
+          </div>
+          <div className="column">
+            <div className="login_btn">
+              <button type="submit">register</button>
+            </div>
+          </div>
+        </form>
+      </div>
 
     </>
   );
