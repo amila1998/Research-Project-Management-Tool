459b8c7532c635d8a04bea8033b9735002589955
amila1998 <amiladevin@gmail.com>
2022-05-21 22:47:59 +0530

    Group Model

---
+++ server/models/groupsModel.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require("mongoose");
+
+const groupSchema = new Schema(
+  {
+    groupName:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    members: [
+        {
+            name: String,
+            user_id:String,
+            isLeader:false
+        }
+    ],
+    supervisor:{
+        name:String,
+        user_id:String,
+        isAccept:{
+            type:Boolean,
+            default:null
+        }
+    },
+    coSupervisor:{
+        name:String,
+        user_id:String,
+        isAccept:{
+            type:Boolean,
+            default:null
+        }
+    },
+    panelMember:{
+        name:String,
+        user_id:String
+    },
+    level:{
+        type:Number,
+        default:0
+    }
+
+
+
+    
+  
+},
+{ timestamps: true }
+);
+
+const Groups = model("Groups", groupSchema);
+
+module.exports = Groups;
