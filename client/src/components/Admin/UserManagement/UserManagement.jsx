f2846504c758e0dae3c3bbbd1ffccc2e0786bb7f
amila1998 <amiladevin@gmail.com>
2022-05-24 01:51:32 +0530

    User Management 4

--- client/src/components/Admin/UserManagement/UserManagement.jsx
+++ client/src/components/Admin/UserManagement/UserManagement.jsx
@@ -8,58 +8,31 @@ import ViewUserDetails from '../../utils/AdminViewUser/ViewUserDetails';
 
 
 import './usermanagement.scss';
-// import { styled } from '@mui/material';
 
-// const BootstrapButton = styled(Button)({
-//     boxShadow: 'none',
-//     textTransform: 'none',
-//     fontSize: 16,
-//     padding: '6px 12px',
-//     border: '1px solid',
-//     lineHeight: 1.5,
-//     color:'#000000',
-//     borderColor: '#ff6a06',
-//     fontFamily: [
-//       '-apple-system',
-//       'BlinkMacSystemFont',
-//       '"Segoe UI"',
-//       'Roboto',
-//       '"Helvetica Neue"',
-//       'Arial',
-//       'sans-serif',
-//       '"Apple Color Emoji"',
-//       '"Segoe UI Emoji"',
-//       '"Segoe UI Symbol"',
-//     ].join(','),
-//     '&:hover': {
-//       backgroundColor: '#ff6a06',
-//       borderColor: '#ff6a06',
-//       boxShadow: 'none',
-//     },
-   
-//   });
-
-// const Transition = React.forwardRef(function Transition(props, ref) {
-//     return <Slide direction="up" ref={ref} {...props} />;
-//   });
-  
   
 
 const UserManagement = () => {
-    const [isVerify,setIsVeryfiy]=useState(false);
+    const [isVerify,setIsVeryfiy]=useState("");
     const [search,setSearch]=useState("");
-    const [sort,setSort]=useState("asc");
+    const [sort,setSort]=useState("");
     const [role,setRole]=useState("");
     const [users,setUsers]=useState([]);
-    
 
+    
+    
+    const handleCategory = e => {
+        setRole(e.target.value)
+        setSearch('')
+    }
 
     
   
     useEffect(() => {
+        console.log(search," ",isVerify," ",sort," ",role," " );
       const getAllUsers=async()=>{
         try {
-          const res = await axios.get(`/api/admin/getallUsers?isVerify=${isVerify}&createdAt${sort}&role=${role}&keyword=${search}`,{
+        
+          const res = await axios.get(`/api/admin/getallUsers?isVerify=${isVerify}&createdAt=${sort}&role=${role}&keyword=${search}`,{
             withCredentials:true
           });
           setUsers(res.data.users);
@@ -68,13 +41,48 @@ const UserManagement = () => {
         }
       }
       getAllUsers();
-    }, [])
+    }, [isVerify,search,sort,role])
 
     console.log(users);
   return (
       <>
       <h1>User Management</h1>
       <div className='table-page'>
+      <div className="filter_menu">
+            <div className="row1">
+                <span>Filters : </span>
+                <select name="category" value={role} onChange={e => setRole(e.target.value)} >
+                    <option value='' selected>All Users</option>
+                    <option value='admin'>Admin</option>
+                    <option value='supervisor'>Supervisor</option>
+                    <option value='cosupervisor'>Co Supervisor</option>
+                    <option value='student'>Student</option>
+                 
+                    
+                </select>
+            </div>
+
+            <input type="text" value={search} placeholder="Search by username or email..."
+            onChange={e => setSearch(e.target.value.toLowerCase())} />
+            <div className="row1">
+                <span>Verification : </span>
+                <select name="category" value={isVerify} onChange={e => setIsVeryfiy(e.target.value)} >
+                    <option value='' selected>All</option>
+                    <option value='true'>Verified</option>
+                    <option value='false'>Not Verified</option>
+                 
+                    
+                </select>
+            </div>
+            <div className="row1 sort">
+                <span>Sort By : </span>
+                <select value={sort} onChange={e => setSort(e.target.value)} >
+                    <option value=''>Oldest</option>
+                    <option value='desc'>Newest</option>
+                </select>
+            </div>
+        </div>
+
             <h4>You have {users.length} users</h4>
 
             <table>
@@ -86,6 +94,7 @@ const UserManagement = () => {
                         <th>Role</th>
                         <th>Verification</th>
                         <th></th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -96,11 +105,12 @@ const UserManagement = () => {
                                 <td>{new Date(items.createdAt).toLocaleDateString()}</td>
                                 <td>{items.email}</td>
                                 <td>{items.role}</td>
-                                <td>{items.isverify?"true":'false'}</td>
+                                <td>{items.isverify?<><button className='btn btn-outline-danger'>Unverify</button></>:<><button className='btn btn-outline-success'>Verify</button></>}</td>
                                 <td>  
                                     <ViewUserDetails data={items}/>
                                    
                                 </td>
+                                <td><button className='btn btn-outline-danger'>Delete</button></td>
                             </tr>
                         ))
                     }
--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -2,58 +2,64 @@ import React, { useContext } from 'react'
 import { AuthContext } from '../../context/AuthContext';
 import Avatar from '../Avatar/Avatar';
 import "./profile.scss";
+import unverify from "../../assets/img/unverify.png"
+import verify from "../../assets/img/verify.png"
 
 const Profile = () => {
     const { user, isCoSuprevisor,isPanelMember,isSupervisor } = useContext(AuthContext);
     console.log(user);
   return (
     <div className='myprofile'>
+      <h1>User Profile{user.isverify?<><img type="button" src={verify} class="fa-solid fa-badge-check btn verfybatch" data-toggle="tooltip" data-placement="top" title="This is a Verification of Admin"/>
+</>:<>
+<img type="button" src={unverify} class="fa-solid fa-badge-check btn verfybatch" data-toggle="tooltip" data-placement="top" title="This is a Verification of Admin"/>
+</>}</h1><br/>
         <div className='profile_avatar'>
         <div className="profile_avatar-wrapper" >
             <Avatar />
         </div>
         </div>
         
-        <div className='row1'>
-          <div className='col1   '><h5>Name</h5></div>
-          <div className='col1'>:</div>
-          <div className='col1'>{user.name}</div>
+        <div className='row'>
+          <div className='col   '><h5>Name</h5></div>
+          <div className='col'>:</div>
+          <div className='col'>{user.name}</div>
         </div>
-        <div className='row1'>
-          <div className='col1   '><h5>Email</h5></div>
-          <div className='col1'>:</div>
-          <div className='col1'> {user.email}</div>
+        <div className='row'>
+          <div className='col   '><h5>Email</h5></div>
+          <div className='col'>:</div>
+          <div className='col'> {user.email}</div>
         </div>
-        <div className='row1'>
-          <div className='col1   '><h5>Role</h5></div>
-          <div className='col1'>:</div>
-          <div className='col1'>{user.role}</div>
+        <div className='row'>
+          <div className='col   '><h5>Role</h5></div>
+          <div className='col'>:</div>
+          <div className='col'>{user.role}</div>
         </div>
-        <div className='row1'>
-          <div className='col1   '><h5>Gender</h5></div>
-          <div className='col1'>:</div>
-          <div className='col1'>{user.gender}</div>
+        <div className='row'>
+          <div className='col   '><h5>Gender</h5></div>
+          <div className='col'>:</div>
+          <div className='col'>{user.gender}</div>
         </div>
         {user.role=="student"&&<>
-        <div className='row1'>
-          <div className='col1   '><h5>Faculty</h5></div>
-          <div className='col1'>:</div>
-          <div className='col1'>{user.student.faculty}</div>
+        <div className='row'>
+          <div className='col   '><h5>Faculty</h5></div>
+          <div className='col'>:</div>
+          <div className='col'>{user.student.faculty}</div>
         </div>
-        <div className='row1'>
-          <div className='col1'><h5>Degree</h5></div>
-          <div className='col1'>:</div>
-          <div className='col1'>{user.student.degree}</div>
+        <div className='row'>
+          <div className='col'><h5>Degree</h5></div>
+          <div className='col'>:</div>
+          <div className='col'>{user.student.degree}</div>
         </div>
-        <div className='row1'>
-          <div className='col1   '><h5>Specialization</h5></div>
-          <div className='col1'>:</div>
-          <div className='col1'>{user.student.specialization}</div>
+        <div className='row'>
+          <div className='col   '><h5>Specialization</h5></div>
+          <div className='col'>:</div>
+          <div className='col'>{user.student.specialization}</div>
         </div>
-        <div className='row1'>
-          <div className='col1   '><h5>Batch</h5></div>
-          <div className='col1'>:</div>
-          <div className='col1'>{user.student.batch}</div>
+        <div className='row'>
+          <div className='col   '><h5>Batch</h5></div>
+          <div className='col'>:</div>
+          <div className='col'>{user.student.batch}</div>
         </div>
         
         </>}
