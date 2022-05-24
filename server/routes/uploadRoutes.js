0ef012660bc3ebe0203c5082666b9eea9968c438
amila1998 <amiladevin@gmail.com>
2022-05-24 15:05:26 +0530

    File uploading 1

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -4,6 +4,9 @@ const uploadImage = require("../middlewares/uploadImage");
 const auth = require("../middlewares/auth");
 const uploadController = require("../controllers/uploadController");
 
+const fileUploadController = require("../controllers/fileUploadController");
+
+
 route.post(
   "/api/upload",
   uploadImage,
@@ -11,5 +14,11 @@ route.post(
   auth,
   uploadController.uploadAvar
 );
+
+route.post(
+  "/api/fileupload",
+  fileUploadController.uploadFile
+);
+
 
 module.exports = route;
