53688bd085b066e6c14a9e1bcf3873c69e36c8fb
amila1998 <amiladevin@gmail.com>
2022-05-27 19:36:48 +0530

    Marking Schema Management step 1

---
+++ client/src/components/Admin/MarkingShemaManagement/MarkingShemaManagement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MarkingShemaManagement = () => {
+  return (
+    <div>MarkingShemaManagement</div>
+  )
+}
+
+export default MarkingShemaManagement
--- client/src/components/Admin/SubmissionTypeManagement/Redux/Axios/event.js
+++ client/src/components/Admin/SubmissionTypeManagement/Redux/Axios/event.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+axios.defaults.withCredentials = true;
 export const event = axios.create({
     baseURL: "/api/events"
   });
