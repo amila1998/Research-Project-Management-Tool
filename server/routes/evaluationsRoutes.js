b88272d585d8fff3352cb144c474e5a2a3add63f
Kevinwijesooriya <81944276+Kevinwijesooriya@users.noreply.github.com>
2022-06-02 02:08:24 +0530

    evaluate submission bug fixes

--- server/routes/evaluationsRoutes.js
+++ server/routes/evaluationsRoutes.js
@@ -7,6 +7,7 @@ const admin = require("../middlewares/admin");
 route.post('/api/evaluation/add', auth, admin, evaluationController.addEvaluation);
 route.get('/api/evaluation/getAll', auth, admin, evaluationController.getAll);
 route.get('/api/evaluation/get/:id', auth, admin, evaluationController.getOne);
+route.post('/api/evaluation/getLevel', auth, admin, evaluationController.getLevel);
 route.post('/api/evaluation/update/:id', auth, admin, evaluationController.updateEvaluation);
 route.delete('/api/evaluation/delete/:id', auth, admin, evaluationController.deleteEvaluation);
 
