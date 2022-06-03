1393d2df514e28af89b8f5728405acee68f42fbc
sean-udayantha <seanudayantha@mail.com>
2022-06-03 22:17:34 +0530

    add CoSupervisor

--- server/routes/submssionRouter.js
+++ server/routes/submssionRouter.js
@@ -4,11 +4,13 @@ const  submssionController= require("../controllers/submssionController");
 const auth = require("../middlewares/auth");
 const admin = require("../middlewares/admin");
 const supervisor = require("../middlewares/supervisor");
+const coSupervisor= require("../middlewares/coSupervisor");
 
 route.post('/api/submssion/add',auth,submssionController.addSubmssion);
 route.get('/api/submssion/getAll',submssionController.getAll);
 route.get('/api/submssion/getOne/:id',submssionController.getOne);
 route.get('/api/submssion/getSupervisorsSub',auth,supervisor,submssionController.getSupervisorsSub);
+route.get('/api/submssion/getCoSupervisorsSub',auth,coSupervisor,submssionController.getCoSupervisorsSub);
 route.get('/api/submssion/getSubmitted/:groupId/:eventId',submssionController.getSubmitted);
 route.put('/api/submssion/updateSubmssion/:id',submssionController.updateSubmssion);
 module.exports = route;
