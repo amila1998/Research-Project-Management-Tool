521cf62fd9fe4cc9cf450bf5237c161f8b27f23c
sean-udayantha <seanudayantha@mail.com>
2022-06-03 19:36:02 +0530

    edit student dashboard

--- client/src/components/Student/DashBoard/DashBoard.jsx
+++ client/src/components/Student/DashBoard/DashBoard.jsx
@@ -4,11 +4,25 @@ import SubmissionDashBoard from '../StudentSubmission/SubmissionDashBoard';
 
 
 const DashBoard = ({group}) => {
+// console.log("🚀 ~ file: DashBoard.jsx ~ line 7 ~ DashBoard ~ group", group?.level)
 
   
   return (
-    <div>Welcome To Student DashBoard
-    <SubmissionDashBoard myGroup={group}/>
+    <div><div className="Dash">Welcome To Student DashBoard</div>
+            {group?.level===0&&<><div  className='nextStep'>Next Step:  Register Your Topic under the Topic Registraion</div></>}
+            {group?.level===1&&<><div className='nextStep'>Next Step: Request a Supervisor</div></>}
+            {group?.level===2&&<><div className='warnStep'>Wait for the Supervisor Response.</div></>}
+            {group?.level===-1&&<><div className='backStep'>Please Try another Supervisor</div></>}
+            {group?.level===3&&<><div className='warnStep'>Wait for the Topic Acception a from Topic Evaluvation Panal Member</div></>}
+            {group?.level===-2&&<><div className='backStep'>Please Try another Topic</div></>}
+            {group?.level===4&&<><div className='nextStep'>Next Step: Request a Co Supervisor</div></>}
+            {group?.level===5&&<><div className='warnStep'>Wait for the Co Supervisor Response.</div></>}
+            {group?.level===-3&&<><div className='backStep'>Please Try another Co Supervisor</div></>}
+            {group?.level===6&&<><div className='warnStep'>Wait for Assigning the Panal Member from Admin.</div></>}
+            {group?.level===7&&<><div className='nextStep'>COOL !!! , Finally Your Group is Complete all Task. Now you can Submit your Submission before the Deadline</div><br/><SubmissionDashBoard myGroup={group}/></>}
+    
+    
+    
 
     </div>
   )
