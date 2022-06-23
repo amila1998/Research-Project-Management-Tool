e38eedfec6ea86b0889caef5705e78a1b33ac851
amila1998 <amiladevin@gmail.com>
2022-06-23 15:24:52 +0530

    heroku 2

--- server/server.js
+++ server/server.js
@@ -87,6 +87,8 @@ app.use(express.static(path.join(__dirname, '/client/dist')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/client/dist/index.html'))
 );
+
+
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
 // });
